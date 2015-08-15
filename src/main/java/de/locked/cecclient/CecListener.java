c25969dbabeb8625886528175626e203cb1e936f
Franz Graf <info@Locked.de>
2015-08-15 16:49:49 +0200

    changed CEC Reader

--- src/main/java/de/locked/cecclient/CecListener.java
+++ src/main/java/de/locked/cecclient/CecListener.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,18 +29,28 @@ import java.util.logging.Logger;
 public class CecListener extends Thread implements AutoCloseable {
 
     private static final Logger log = Logger.getLogger(CecListener.class.getName());
-    private static final String COMMAND = "/usr/local/bin/cec-client -d 8 -t prta -o Wetter";
     private static final int SLEEP_BETWEEN_RETRIES = 1000;
+    private static final int EMPTY_STREAM_DELAY = 500;
 
+    private final String command;
     private final Parser parser = new Parser();
     private final List<CallBackListener> listener = new LinkedList<>();
     private Process process;
-    private InputStream inputStream;
+    InputStream inputStream;
+    InputStream errorStream;
     int maxRetryCount = 10;
     boolean keepAlive = true;
 
-    public CecListener() {
+    public CecListener(String command) {
+        this.command = command;
         setDaemon(true);
+    }
+
+    void openStreams() throws IOException {
+        ProcessBuilder pb = new ProcessBuilder(command.split(" "));
+        process = pb.start();
+        inputStream = process.getInputStream();
+        errorStream = process.getErrorStream();
     }
 
     @Override
@@ -47,30 +58,26 @@ public class CecListener extends Thread implements AutoCloseable {
         int retryCount = 1;
         do {
             try {
-                log.info("Starting cec-listener on command line. Attempt " + retryCount);
-                inputStream = openStream();
+                log.log(Level.INFO, "Starting cec-listener on command line. Attempt {0}", retryCount);
+                openStreams();
+                CecStreamConsumer is = new CecStreamConsumer("InputStream", inputStream, s -> fireEvent(parser.toKeyCode(s)));
+                CecStreamConsumer es = new CecStreamConsumer("ErrorStream", errorStream);
+                do {
+                    is.maybeReadStream();
+                    es.maybeReadStream();
+                    sleep(EMPTY_STREAM_DELAY); // don't hammer the poor raspi down
+                } while (keepAlive && !isInterrupted());
 
-                byte[] b = new byte[1];
-                StringBuilder sb = new StringBuilder(60);
-                while (inputStream.read(b) >= 0) {
-                    retryCount = 1;
-                    String s = new String(b);
-                    if (s.equals("\n")) {
-                        KEvent keyCode = parser.toKeyCode(sb.toString());
-                        fireEvent(keyCode);
-                        sb = new StringBuilder(60);
-                    } else {
-                        sb.append(s);
-                    }
-                }
             } catch (IOException ex) {
-                log.warning("Error reading stream from commandline!");
+                log.warning("Error reading CEC stream from commandline!");
+            } catch (InterruptedException ex) {
+                log.log(Level.SEVERE, "Thread interrupted", ex);
             } finally {
                 close();
             }
 
-            // set asleep after an error. Maybe the call is simply invalid. In this case I want to avoid going into an
-            // infinite loop
+            // set asleep after an error. Maybe the call is simply invalid. In this case I want to avoid going
+            // into an infinite loop
             try {
                 sleep(SLEEP_BETWEEN_RETRIES);
             } catch (InterruptedException ex) {
@@ -78,12 +85,6 @@ public class CecListener extends Thread implements AutoCloseable {
                 return;
             }
         } while (keepAlive && !isInterrupted() && retryCount++ < maxRetryCount);
-    }
-
-    InputStream openStream() throws IOException {
-        ProcessBuilder pb = new ProcessBuilder(COMMAND.split(" "));
-        process = pb.start();
-        return process.getInputStream();
     }
 
     void fireEvent(KEvent key) {
@@ -106,12 +107,55 @@ public class CecListener extends Thread implements AutoCloseable {
         if (process != null) {
             process.destroy();
         }
+        closeStream(inputStream);
+        closeStream(errorStream);
+    }
+
+    private void closeStream(InputStream is) {
         try {
-            if (inputStream != null) {
-                inputStream.close();
+            if (is != null) {
+                is.close();
             }
         } catch (IOException ex) {
             log.log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
+class CecStreamConsumer {
+
+    private static final Logger log = Logger.getLogger(CecStreamConsumer.class.getName());
+    private final String label;
+    private final InputStream is;
+    private final Consumer<String> consumer;
+    private final byte[] b = new byte[1];
+
+    CecStreamConsumer(String label, InputStream stream, Consumer<String> consumer) {
+        this.label = label;
+        this.is = stream;
+        this.consumer = consumer;
+    }
+
+    CecStreamConsumer(String label, InputStream stream) {
+        this(label, stream, i -> {
+        });
+    }
+
+    void maybeReadStream() throws IOException {
+        if (is.available() > 0) {
+            StringBuilder sb = new StringBuilder(60);
+            while (is.available() > 0 && is.read(b) >= 0) {
+                String s = new String(b).intern();
+                if (s.equals("\n")) {
+                    if(sb.length() > 0){
+                        log.log(Level.FINE, "CEC command ''{0}'': {1}", new Object[]{label, s});
+                        consumer.accept(sb.toString().trim());
+                        sb = new StringBuilder(60);
+                    }
+                } else {
+                    sb.append(s);
+                }
+            }
         }
     }
 }
